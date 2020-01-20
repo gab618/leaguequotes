dd7ca577d682fe5318891694648bc2eba3c41f9f
Gabriel Rocha <knuckles4x@gmail.com>
2020-01-20 10:05:02 -0300

    Lista de quotes dos campeoes

---
+++ src/quoteList.js
@@ -0,0 +1,18 @@
+export const quoteList = [
+    { quote: "Seu lugar é no museu", author: "Ezreal" },
+    { quote: "Vira-latas imundos ficam na coleira", author: "Thresh" },
+    { quote: "Num massacre floresço, como uma flor no amanhecer", author: "Jhin" },
+    { quote: "O que não mata ainda não terminou", author: "Twitch" },
+    { quote: "Nunca um passo em falso", author: "Jayce" },
+    { quote: "Calma, amigo", author: "Graves" },
+    { quote: "A sorte sempre me sorri", author: "Twisted Fate" },
+    { quote: "Sua vida não tinha valor antes de mim", author: "Jhin" },
+    { quote: "A palavra certa corta mais profundamente que uma faca", author: "Camille" },
+    { quote: "Quem precisa de mapas?", author: "Ezreal" },
+    { quote: "O coração é o músculo mais forte", author: "Braum" },
+    { quote: "Eu luto por um futuro iluminado", author: "Jayce" },
+  ]
+
+export default {
+    quoteList,
+}
