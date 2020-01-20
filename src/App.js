836a24f7bdb82b13531d3d91ca29649240618e6d
Gabriel Rocha <knuckles4x@gmail.com>
2020-01-20 10:03:13 -0300

    Projeto League Quotes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { quoteList } from './quoteList';
 
 function App() {
+   
+  const [quote, setQuote] = useState('');
+  function getRandomQuote(quoteList){
+    setQuote(quoteList[Math.floor(Math.random() * quoteList.length)]);
+    return quote;
+  }
+
+  if(!quote){
+    getRandomQuote(quoteList);
+  }
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+      <p className="Quote">
+        {`"${quote.quote}"`} - <span className="Author">{`${quote.author}`}</span>
+      </p>
+      <a className="twitter-share-button"
+          href={`https://twitter.com/intent/tweet?text="${quote.quote}" - ${quote.author}`}
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      >
+    Tweet</a>
     </div>
   );
 }
